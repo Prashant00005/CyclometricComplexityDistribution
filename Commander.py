6e50ab95ea0a157d66b8fcb5d062854cd5fe7cad
Prashant Aggarwal <aggarwap@tcd.ie>
2017-12-17 22:57:19 +0000

    Rodon command not workig.. Updated with Lizard

--- Commander.py
+++ Commander.py
@@ -9,16 +9,16 @@ api = Api(app)
 
 
 class FindCyclomaticComp(Resource):     #To find commits and find cyclomatic aomplexities
-    def __init__(self):  
+    def __init__(self):
         #Initializing global object of Commander class
-        global Commander_Obj 
+        global Commander_Obj
         self.server = Commander_Obj
-        
-        super(FindCyclomaticComp, self).__init__() 
+
+        super(FindCyclomaticComp, self).__init__()
         self.ProcessRequest = reqparse.RequestParser()
 
-       
-        self.ProcessRequest.add_argument('CommitSub', type=str, location = 'json')  
+
+        self.ProcessRequest.add_argument('CommitSub', type=str, location = 'json')
         self.ProcessRequest.add_argument('CycloComp', type=float, location='json')
 
     def get(self):      #If Request is Get
@@ -26,47 +26,59 @@ class FindCyclomaticComp(Resource):     #To find commits and find cyclomatic aom
         if self.server.Current_Subjects < self.server.Subjects:
             time.sleep(0.1)
             return {'msg': -2}
-        
-        if Length_List_Commits == 0: 
+
+        if Length_List_Commits == 0:
             return {'msg': -1}
-        
+
         tempVal = self.server.List_Commits[0]
-        del self.server.List_Commits[0] 
+        del self.server.List_Commits[0]
         print("Resp: {}".format(tempVal))
         return {'msg':tempVal}
 
 
     def post(self):     #If Request is Post
-        
-        args = self.ProcessRequest.parse_args() 
+
+        args = self.ProcessRequest.parse_args()
         Length_Total_List_CC = len(self.server.Total_List_CC)
+
+        print(Length_Total_List_CC)
         print("CommitSub got is {}".format(args['CommitSub']))
         print("CycloComp got is {}".format(args['CycloComp']))
 
         self.server.Total_List_CC.append({'CS':args['CommitSub'], 'CC':args['CycloComp']})
-       
+
         print(self.server.Total_List_CC)
         print(self.server.List_Commits)
-        
-        if Length_Total_List_CC == self.server.totalCommits: 
-            LastTime = time.time() - self.server.startTime
-            
+
+
+
+
+        print("Value of Length_Total_List_CC is ")
+        print(Length_Total_List_CC)
+        print("Value of self.server.totalCommits-1 is")
+        print(self.server.totalCommits-1)
+
+        if Length_Total_List_CC == (self.server.totalCommits-1):
+            print("Inside true condition")
+            print ("Value of Length_Total_List_CC is ")
+            LastTime = time.time() - self.server.timer
+
             print("End in {} seconds".format(LastTime))
             print(len(self.server.Total_List_CC))
-            
+
             Average_CC = 0
-        
+
             for a in self.server.Total_List_CC:
-            
+
                 if a['CC'] > 0:
                     Average_CC = Average_CC + a['CC']
                 else:
                     print("Commit {} has no computable files".format(a['CS']))
-            
+
             Average_CC = Average_CC / Length_Total_List_CC
-            
+
             print("The Final Cyclomatic Complexity of the given repository is: {}".format(Average_CC))
-        
+        print ("If condition is false")
         return {'success':True}
 
 
@@ -75,31 +87,31 @@ api.add_resource(FindCyclomaticComp, "/Cyclo", endpoint="Cyclo")
 
 
 class FetchRepo(Resource):      #To get the Repository Information for subjects
-   
-    def __init__(self):  
+
+    def __init__(self):
         #Initializing global object of Commander class
-        global Commander_Obj 
+        global Commander_Obj
         self.server = Commander_Obj
-        
+
         super(FetchRepo, self).__init__()
-        
+
         self.ProcessRequest = reqparse.RequestParser()
 
-        # Setting Parameters for JSON 
-        self.ProcessRequest.add_argument('FetchStatus', type=int, location = 'json') 
+        # Setting Parameters for JSON
+        self.ProcessRequest.add_argument('FetchStatus', type=int, location = 'json')
         self.ProcessRequest.add_argument('FindCyclo', type=float, location='json')
 
     def get(self):
         args = self.ProcessRequest.parse_args()
-        if args['FetchStatus'] == False:  #Check repository is not pulled yet 
-           
+        if args['FetchStatus'] == False:  #Check repository is not pulled yet
+
             return {'repo': "https://github.com/Prashant00005/Distributed_File_Systems"}
-        
+
         if args['FetchStatus'] == True:
             self.server.Current_Subjects = self.server.Current_Subjects + 1
             if self.server.Current_Subjects == self.server.Subjects:
                 #Starting Timer
-                self.server.timer = time.time() 
+                self.server.timer = time.time()
             print("Number of Subject : {}".format(self.server.Current_Subjects))
 
 
@@ -107,63 +119,64 @@ api.add_resource(FetchRepo, "/repo", endpoint="repo")
 
 
 class CommanderServer():
-    
+
     def __init__(self):
-    
+
         self.Subjects = input("How many workers do you want to create:")
-        
+
         self.Subjects = int(self.Subjects)  #Stores the number of workers created
         self.Current_Subjects = 0  # Initializing the no. of subjects that have connected to server
-        self.timer = 0.0  
-       
-        
+        self.timer = 0.0
+
+
         Username_Repo = input("Enter Github Username ")
         Length_Username_Repo = len(Username_Repo)
-        
+
         if  Length_Username_Repo != 0:     #To check for null pointer
             Password_Repo = getpass.getpass("Type your Github password (input is hidden): ")
-        
+
         Curr_RepoPage = 1  #initialize page with First page
-        
+
         flag = True  #to check for more number of pages
-         
+
         self.List_Commits = []  # List of all commits
-        
+
         while flag:
-            
-            
-        
+
+
+
             if Length_Username_Repo == 0:
-                r = requests.get("https://api.github.com/repos/Prashant00005/Distributed_File_Systems/commits?page={}&per_page=100".format(Curr_RepoPage))  
+                r = requests.get("https://api.github.com/repos/Prashant00005/Distributed_File_Systems/commits?page={}&per_page=100".format(Curr_RepoPage))
             else:
                 r = requests.get("https://api.github.com/repos/Prashant00005/Distributed_File_Systems/commits?page={}&per_page=100".format(Curr_RepoPage), auth=(Username_Repo, Password_Repo))
-                
+
             string_json = json.loads(r.text)
             Length_string_json = len(string_json)
-            
+
             if  Length_string_json < 2:
-             
+
                 flag = False
                 print("Iteration of all pages done!!")
-            
+
             else:
-                
+
                 for sub_json in string_json:
                     self.List_Commits.append(sub_json['sha'])  #Keep on adding to get total
                     print("Commit ID  given is Sha: {}".format(sub_json['sha']))
-                    
+
                 print("\n")
                 print("\n")
                 Curr_RepoPage = Curr_RepoPage + 1
-        
+
         Length_List_Commits = len(self.List_Commits)
         self.totalCommits = Length_List_Commits
         self.Total_List_CC = []
-        
+
         print("Total number of commits are : ".format(self.totalCommits))
 
 
 if __name__ == "__main__":
-    app.run(port=8084)
+
     Commander_Obj = CommanderServer()  #creates object of Commander Server class
+    app.run(port=9020)
     
--- Subject.py
+++ Subject.py
@@ -9,48 +9,62 @@ def run():
 
     req = requests.get("http://{}:{}/repo".format(IP_Commander,Port_Commander), json={'FetchStatus': False})
     string_json = json.loads(req.text)
+    print ("Printing")
+    print (string_json)
     Url_Repository = string_json['repo']
+
     subprocess.call(["bash", "SubjectScript.sh", Url_Repository])
 
-    req = requests.get("http://{}:{}/repo".format(IP_Commander,Port_Commander), json={'FetchStatus': True}) 
+    req = requests.get("http://{}:{}/repo".format(IP_Commander,Port_Commander), json={'FetchStatus': True})
 
     Pending_Commit = True
-    
+
     while Pending_Commit:
-        
-        req = requests.get("http://{}:{}/Cyclo".format(IP_Commander,Port_Commander))  
+
+        req = requests.get("http://{}:{}/Cyclo".format(IP_Commander,Port_Commander))
         string_json = json.loads(req.text)
         print(string_json)
         print("Response: {}".format(string_json['msg']))
-        if string_json['msg'] == -2: 
+        if string_json['msg'] == -2:
 
            print("Checking")
-           
+
         else:
-           
+
             if string_json['msg'] == -1:
                 print("All done!!")
                 break
             subprocess.call(["bash", "SubjectCommit.sh", string_json['msg']])
-            
-            RadonOutput = subprocess.check_output(["radon", "cc", "-s", "-a" , "workerData"])
-            
-            radonCCOutput = RadonOutput.decode("utf-8")  
-            
+            #exit()
+
+            RadonOutput = subprocess.check_output(["lizard","SubjectCommitTest"])
+
+            radonCCOutput = RadonOutput.decode("utf-8")
+
             print(radonCCOutput)
-            
-            CC_Average_Start = radonCCOutput.rfind("(")
-            
-            if radonCCOutput[CC_Average_Start+1:-2] == "":
+
+
+
+            lastline = radonCCOutput.splitlines()[-1:]
+            CC_Average = lastline[0].split()[2]
+            print ("Value of last line is")
+            print (lastline)
+            print ("Value of CC_Average is")
+            print (CC_Average)
+
+            if CC_Average=="":
                 print("Error! No file needed..")
-                
-                req = requests.post("http://{}:{}/cyclomatic".format(IP_Commander,Port_Commander),
+
+                req = requests.post("http://{}:{}/Cyclo".format(IP_Commander,Port_Commander),
                                   json={'CommitSub': string_json['msg'], 'CycloComp': -1})
             else:
-                CC_Average = float(radonCCOutput[CC_Average_Start+1:-2]) 
-                req = requests.post("http://{}:{}/cyclomatic".format(IP_Commander,Port_Commander),
+                print(CC_Average)
+                req = requests.post("http://{}:{}/Cyclo".format(IP_Commander,Port_Commander),
                                   json={'CommitSub': string_json['msg'], 'CycloComp': CC_Average})
-            Commit_Count = Commit_Count + 1 
+
+            Commit_Count = Commit_Count + 1
+
+
 
 if __name__ == "__main__":
     run()
